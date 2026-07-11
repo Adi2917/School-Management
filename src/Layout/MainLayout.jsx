439cc7f2582e07382417dd63b4d63c41b10ee561
Adi2917 <samratadi4321@gmail.com>
2026-07-11 23:13:17 +0530

    Polish student cards and education backgrounds

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./MainLayout.css";
 import Header from "../Components/Header";
+import EducationAtmosphere from "../Components/EducationAtmosphere";
 
 export default function MainLayout() {
   return (
     <main className="app-content">
+      <EducationAtmosphere />
       <Header />
       <Outlet />
     </main>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.css";
 import "./theme.css";
 import "./premium.css";
+import "./final-polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
